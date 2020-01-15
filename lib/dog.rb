934cb9d6d9fc4b7b07491755f039d1bcc4978e7c
yaoyu bao <yaobao5354@gmail.com>
2020-01-15 22:26:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,7 @@ class Dog
     SQL
 
     DB[:conn].execute(sql, self.name, self.breed)
-    self
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 
   def self.create(attribute)
