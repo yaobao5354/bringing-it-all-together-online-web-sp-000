1da8811753f9abf9cff7fa26fbe9382076cbf758
yaoyu bao <yaobao5354@gmail.com>
2020-01-15 22:06:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,11 +72,11 @@ class Dog
      WHERE name = "#{attribute[:name]}" AND breed = "#{attribute[:breed]}"
     SQL
     row = DB[:conn].execute(sql)
-    binding.pry
     if row == []
       self.create(attribute)
     else
       self.find_by_id(row[0][0])
+      binding.pry
     end
 
   end
