8b2516f5f76aaca684e82d025d0f046bc358eef1
yaoyu bao <yaobao5354@gmail.com>
2020-01-15 22:32:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,7 +86,7 @@ class Dog
 
   def update
     sql = "UPDATE dogs SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
